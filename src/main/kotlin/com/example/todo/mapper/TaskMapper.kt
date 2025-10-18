0abc0b994e317abcae10a2c747945be5b6dc7a75
minami <south614fruit@yahoo.co.jp>
2025-10-18 23:53:29 +0900

    insertメソッドの修正

--- src/main/kotlin/com/example/todo/mapper/TaskMapper.kt
+++ src/main/kotlin/com/example/todo/mapper/TaskMapper.kt
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Param
 interface TaskMapper {
     fun findAll(): Array<Task>
     fun findTaskById(@Param("id") id: Long): Task?
-    fun insertTask(@Param("task")task: Task): Int
+    fun insertTask(task: Task): Int
     fun updateTaskStatus(@Param("id") id: Long, @Param("status") status: Int): Int
     fun updateTask(@Param("id") id: Long, @Param("task") task: Task): Int
     fun deleteTask(@Param("id") id: Long): Int
